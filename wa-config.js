9b3708d863cb58de7c42801f746ce25a6c9e3150
essentialGrowth <123984248+essentialGrowth@users.noreply.github.com>
2026-08-02 19:15:02 -0300

    Update wa-config.js

--- wa-config.js
+++ wa-config.js
@@ -19,10 +19,12 @@
 
 /* Carimbo de versao. Formato: AAAA-MM-DD-NN (NN = edicao do dia).
    BUMP este valor a cada alteracao da lista de numeros. */
-window.WA_CONFIG_VERSION = '2026-07-22-01';
+window.WA_CONFIG_VERSION = '2026-08-02-01';
 
 window.WA_NUMEROS = [
-   '5521910059256'
+   '557597067986',
+   '557598173424',
+   '557599433803'
 ];
 
 /* Mensagem que ja vem preenchida ao abrir o WhatsApp */
